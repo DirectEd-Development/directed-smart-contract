39b27a82d674f2f8815ba39b7279b254e3470a23
Rohan Mitta <mitta.rohan@gmail.com>
2022-09-15 18:44:37 +0100

    created the file and began setup

---
+++ src/contract.hs
@@ -0,0 +1,23 @@
+-- {-# LANGUAGE DataKinds           #-}
+-- {-# LANGUAGE DeriveAnyClass      #-}
+-- {-# LANGUAGE DeriveGeneric       #-}
+-- {-# LANGUAGE FlexibleContexts    #-}
+-- {-# LANGUAGE NoImplicitPrelude   #-}
+-- {-# LANGUAGE OverloadedStrings   #-}
+-- {-# LANGUAGE ScopedTypeVariables #-}
+-- {-# LANGUAGE TemplateHaskell     #-}
+-- {-# LANGUAGE TypeApplications    #-}
+-- {-# LANGUAGE TypeFamilies        #-}
+-- {-# LANGUAGE TypeOperators       #-}
+-- {-# LANGUAGE NumericUnderscores  #-}
+-- {-# LANGUAGE NamedFieldPuns      #-}
+
+-- {-# OPTIONS_GHC -fno-ignore-interface-pragmas #-}
+
+
+
+
+
+
+{-# INLINABLE mkValidator #-}
+mkValidator :: 
