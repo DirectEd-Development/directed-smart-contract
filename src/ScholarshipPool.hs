4edac93c4573c0808b6d555a2e60f5d02ce60cfa
Rohan Mitta <mitta.rohan@gmail.com>
2022-10-28 22:24:05 +0100

    Fixed scChooser to pick correct instance!

--- src/ScholarshipPool.hs
+++ src/ScholarshipPool.hs
@@ -15,17 +15,35 @@
 
 {-# OPTIONS_GHC -fno-ignore-interface-pragmas #-}
 
+{-# OPTIONS_GHC -Wno-unused-local-binds #-}
+
 module ScholarshipPool where
 
-import qualified    Contract
+import Scholarship
 import qualified    PlutusTx
 import              Ledger                  hiding (mint, singleton)
 import qualified    Ledger.Typed.Scripts    as Scripts
-import GHC.Base (undefined)
 import PlutusTx.Prelude                     hiding (Semigroup(..), unless)
 import Ledger.Contexts                      as Contexts
 import Ledger.Value                         as Value
 import qualified Plutus.V1.Ledger.Ada as Ada
+import Plutus.Contract                      as Contract
+import Data.Text                            (Text)
+import Plutus.Contract.StateMachine (AsSMContractError, StateMachine)
+import Ledger.Typed.Scripts (TypedValidator)
+import Ledger.Constraints                   as Constraints
+import Control.Lens (review)
+import Plutus.Contract.Request (mkTxContract)
+import Prelude                (Semigroup (..))
+
+
+
+import Plutus.Contract.StateMachine
+
+import           Ledger.Ada             as Ada hiding (divide)
+import Data.Map (Map)
+import Control.Monad (void)
+
 
 
 --Student can consume Acceptance NFT and reference Student Status NFT to create an instance 
@@ -47,15 +65,15 @@ getScriptInputs ctx
     | Nothing <- findOwnInput ctx = [] --This case should be impossible.
 
 {-# INLINABLE mkPoolValidator #-}
-mkPoolValidator :: Contract.Scholarship -> Ledger.ValidatorHash -> PaymentPubKeyHash -> () -> ScriptContext -> Bool
+mkPoolValidator :: Scholarship -> Ledger.ValidatorHash -> PaymentPubKeyHash -> () -> ScriptContext -> Bool
 mkPoolValidator schol sValHash pkh _ ctx = traceIfFalse "doesn't consume acceptance token" consumesAcceptaceToken
                                             && traceIfFalse "doesn't reference student status token" referencesStudentToken
-                                            && traceIfFalse "doesn't create correct scholarship" createsCorrectScholarship 
+                                            && traceIfFalse "doesn't create correct scholarship" createsCorrectScholarship
                                             && traceIfFalse "doesn't withdraw correct amount" withdrawCorrectAmount
     where
         txInfo = scriptContextTxInfo ctx
         valueMinted = txInfoMint txInfo
-        consumesAcceptaceToken = valueOf valueMinted (Contract.sAuthoritySym schol) 
+        consumesAcceptaceToken = valueOf valueMinted (sAuthoritySym schol)
                                 (TokenName $ getPubKeyHash (unPaymentPubKeyHash pkh)) == (-1)
 
         referencesStudentToken = True -- TODO
@@ -73,14 +91,84 @@ mkPoolValidator schol sValHash pkh _ ctx = traceIfFalse "doesn't consume accepta
         adaDeposited = Ada.fromValue valueDeposited
         withdrawCorrectAmount = True --TODO
 
-
-
-
 data Pool
 instance Scripts.ValidatorTypes Pool where
     type instance DatumType Pool = PaymentPubKeyHash
     type instance RedeemerType Pool = ()
 
-typedPoolValidator :: Contract.Scholarship -> Scripts.TypedValidator Pool
-typedPoolValidator = undefined
+typedPoolValidator :: Scholarship -> Scripts.TypedValidator Pool
+typedPoolValidator schol = Scripts.mkTypedValidator @Pool
+    ($$(PlutusTx.compile [|| mkPoolValidator ||])
+        `PlutusTx.applyCode` PlutusTx.liftCode schol
+        `PlutusTx.applyCode` PlutusTx.liftCode (scholarshipValHash schol))
+    $$(PlutusTx.compile [|| wrap ||])
+  where
+    wrap = Scripts.wrapValidator @PaymentPubKeyHash @()
+
+poolValidator :: Scholarship -> Validator
+poolValidator = Scripts.validatorScript . typedPoolValidator
+
+poolValHash :: Scholarship -> Ledger.ValidatorHash
+poolValHash = Scripts.validatorHash . typedPoolValidator
+
+poolScrAddress :: Scholarship -> Ledger.Address
+poolScrAddress = scriptAddress . poolValidator
+
+type PoolSchema =
+        Endpoint "initScholarship" PaymentPubKeyHash
+    .\/ Endpoint "refundPool" ()
+
+--This should:
+-- Pick utxos from the pool enough to make a scholarship and send back minScriptAda. (There is theory about how to pick?)
+-- Use these to initialize the correct scholarship.
+-- Provide 'evidence' - in this case, consumed tokens and referenced tokens - to satisfy the pool script.
+initScholarship :: Scholarship -> PaymentPubKeyHash -> Contract () s Text ()
+initScholarship scholarship pkhRecipient = do
+    let v = lovelaceValueOf $ sAmount scholarship
+    utxos <- utxosAt $ scholarshipScrAddress scholarship
+    pkhOwn <- Contract.ownPaymentPubKeyHash 
+    ownUtxos <- utxosAt $ pubKeyHashAddress pkhOwn Nothing
+    let acceptanceToken = Value.singleton (sAuthoritySym scholarship) (TokenName $ getPubKeyHash (unPaymentPubKeyHash pkhRecipient)) 1
+    return () --TODO
+
+-- | Initialise a state machine with no thread token, using a specified set of possible input utxos. 
+-- Have to deconstruct and add to initScholarship....
+-- runInitialiseNoTT ::
+--     forall w e state schema input.
+--     ( PlutusTx.FromData state
+--     , PlutusTx.ToData state
+--     , PlutusTx.ToData input
+--     , AsSMContractError e
+--     )
+--     => Map TxOutRef ChainIndexTxOut
+--     -- ^ The set of utxos to use as inputs
+
+--     -> TypedValidator (StateMachine state input)
+--     -- ^ The state machine typed validator script
+--     -> state
+--     -- ^ The initial state
+--     -> Value
+--     -- ^ The value locked by the contract at the beginning
+--     -> Contract w schema e state
+-- runInitialiseNoTT utxos typedValidator initialState initialValue = mapError (review _SMContractError) $ do
+--     ownPK <- Contract.ownPaymentPubKeyHash
+--     let constraints = mustPayToTheScript initialState initialValue
+--         lookups = Constraints.typedValidatorLookups typedValidator
+--             <> Constraints.unspentOutputs utxos
+--     utx <- mapError (review _ConstraintResolutionContractError) (mkTxContract lookups constraints)
+--     let adjustedUtx = Constraints.adjustUnbalancedTx utx
+--     -- unless (utx == adjustedUtx) $
+--     --   logWarn @Text $ "Plutus.Contract.StateMachine.runInitialise: "
+--     --                 <> "Found a transaction output value with less than the minimum amount of Ada. Adjusting ..."
+--     submitTxConfirmed adjustedUtx
+--     pure initialState
+
+refundPool :: Scholarship -> () -> Contract () PoolSchema Text ()
+refundPool scholarship () = return () --TODO
+
+poolEndpoints :: Scholarship -> Contract () PoolSchema Text ()
+poolEndpoints scholarship = awaitPromise (init' `select` refund') >> poolEndpoints scholarship
+  where
+    init' = endpoint @"initScholarship" $ initScholarship scholarship
+    refund' = endpoint @"refundPool" $ refundPool scholarship
 
---
+++ src/TestScholarship.hs
@@ -0,0 +1,145 @@
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE NumericUnderscores    #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeFamilies          #-}
+
+module TestScholarship where
+
+import Plutus.Trace.Emulator as Emulator
+import Wallet.Emulator.Wallet
+import Data.Default               (Default (..))
+import PlutusTx.Prelude
+import Ledger.TimeSlot (slotToEndPOSIXTime)
+import Control.Monad
+
+import Scholarship
+import qualified VerifiedByToken
+import Prelude (IO)
+
+basicTest :: IO ()
+basicTest = runEmulatorTraceIO basicTrace
+
+basicTrace :: EmulatorTrace ()
+basicTrace = do 
+    let (w1,w2,w3) = (knownWallet 1,knownWallet 2,knownWallet 3)
+    let pkh1      = mockWalletPaymentPubKeyHash w1
+        pkh2      = mockWalletPaymentPubKeyHash w2 
+        pkh3      = mockWalletPaymentPubKeyHash w3
+        amount    = 40_000_000
+        milestones= 2
+        deadline  = slotToEndPOSIXTime def 20
+
+        scholarship = Scholarship
+            { sAuthority        = pkh2 
+            , sAuthoritySym     = VerifiedByToken.curSymbol pkh2 
+            , sSchool           = pkh3
+            , sSchoolSym        = VerifiedByToken.curSymbol pkh3 -- Unused
+            , sCourseProvider   = pkh3
+            , sCourseProviderSym= VerifiedByToken.curSymbol pkh3
+            , sAmount           = amount
+            , sMilestones       = milestones
+            , sDeadline         = deadline
+            }
+
+    h1 <- activateContractWallet w1 $ endpoints scholarship
+    h2 <- activateContractWallet w2 $ endpoints scholarship
+    h3t <- activateContractWallet w3 VerifiedByToken.endpoints
+        
+    callEndpoint @"init" h2 pkh1 -- Authority Initiates Scholarship for Recipient (pkh1)
+
+    void $ Emulator.waitNSlots 3
+
+    callEndpoint @"mint" h3t (2,pkh1) -- Courseprovider mints 2 milestone completion tokens and sends to recipient.
+
+    void $ Emulator.waitNSlots 3
+
+    callEndpoint @"progress" h1 (ScholarshipDatum pkh1 0)  -- Recipient provides evidence of progress and recieves portion of scholarship.
+
+    void $ Emulator.waitNSlots 3
+
+twoStudentsBasicTest :: IO ()
+twoStudentsBasicTest = runEmulatorTraceIO twoStudentsBasicTrace
+
+twoStudentsBasicTrace :: EmulatorTrace ()
+twoStudentsBasicTrace = do 
+    let (w1,w2,w3,w4) = (knownWallet 1,knownWallet 2,knownWallet 3,knownWallet 4)
+    let pkh1      = mockWalletPaymentPubKeyHash w1 --Student 1
+        pkh2      = mockWalletPaymentPubKeyHash w2 --Authority
+        pkh3      = mockWalletPaymentPubKeyHash w3 --CourseProvider
+        pkh4      = mockWalletPaymentPubKeyHash w4 --Student 2
+        amount    = 40_000_000
+        milestones= 2
+        deadline  = slotToEndPOSIXTime def 20
+
+        scholarship = Scholarship
+            { sAuthority        = pkh2 
+            , sAuthoritySym     = VerifiedByToken.curSymbol pkh2 
+            , sSchool           = pkh3
+            , sSchoolSym        = VerifiedByToken.curSymbol pkh3 -- Unused
+            , sCourseProvider   = pkh3
+            , sCourseProviderSym= VerifiedByToken.curSymbol pkh3
+            , sAmount           = amount
+            , sMilestones       = milestones
+            , sDeadline         = deadline
+            }
+
+    h1 <- activateContractWallet w1 $ endpoints scholarship
+    h2 <- activateContractWallet w2 $ endpoints scholarship
+    h3t <- activateContractWallet w3 VerifiedByToken.endpoints
+    h4 <- activateContractWallet w4 $ endpoints scholarship
+        
+    callEndpoint @"init" h2 pkh1 -- Authority Initiates Scholarship for Recipient 1 (pkh1)
+
+    void $ Emulator.waitNSlots 3
+
+    callEndpoint @"init" h2 pkh4 -- Authority Initiates Scholarship for Recipient 2 (pkh4)
+
+    void $ Emulator.waitNSlots 3
+
+    callEndpoint @"mint" h3t (2,pkh1) -- Courseprovider mints 2 milestone completion tokens and sends to recipient.
+
+    void $ Emulator.waitNSlots 3
+
+    callEndpoint @"progress" h1 (ScholarshipDatum pkh1 0)  -- Recipient provides evidence of progress and recieves portion of scholarship.
+
+    void $ Emulator.waitNSlots 3
+
+refundTest :: IO ()
+refundTest = runEmulatorTraceIO refundTrace
+
+refundTrace :: EmulatorTrace ()
+refundTrace = do 
+
+    let (w1,w2,w3) = (knownWallet 1,knownWallet 2,knownWallet 3)
+    let pkh1      = mockWalletPaymentPubKeyHash w1
+        pkh2      = mockWalletPaymentPubKeyHash w2 
+        pkh3      = mockWalletPaymentPubKeyHash w3
+        amount    = 40_000_000
+        milestones= 2
+        deadline  = slotToEndPOSIXTime def 20
+
+        scholarship = Scholarship
+            { sAuthority        = pkh2 
+            , sAuthoritySym     = VerifiedByToken.curSymbol pkh2 
+            , sSchool           = pkh3
+            , sSchoolSym        = VerifiedByToken.curSymbol pkh3 -- Unused
+            , sCourseProvider   = pkh3
+            , sCourseProviderSym= VerifiedByToken.curSymbol pkh3
+            , sAmount           = amount
+            , sMilestones       = milestones
+            , sDeadline         = deadline
+            }
+    h2 <- activateContractWallet w2 $ endpoints scholarship
+       
+    callEndpoint @"init" h2 pkh1 -- Authority Initiates Scholarship for Recipient
+
+    void $ Emulator.waitNSlots 20
+
+    callEndpoint @"refund" h2 (ScholarshipDatum pkh1 0)
+
+    void $ Emulator.waitNSlots 3
