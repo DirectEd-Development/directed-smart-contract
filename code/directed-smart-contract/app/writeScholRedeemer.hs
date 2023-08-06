59ec1bfc95f2cd02e869aa547cb1ede6dbcec6de
Rohan Mitta <mitta.rohan@gmail.com>
2023-08-06 10:02:10 +0000

    MLab's Comments

--- code/directed-smart-contract/app/writeScholRedeemer.hs
+++ code/directed-smart-contract/app/writeScholRedeemer.hs
@@ -1,12 +1,14 @@
 module Main
     ( main
     ) where
-        
+
 import System.Environment   (getArgs)
 import Scholarship          (ScholarshipRedeemer (ScholarshipRedeemer))
 import Utilities (writeDataToFile)
+import Plutus.V1.Ledger.Api (toData)
 
 main :: IO ()
 main = do
     [file, refundString] <- getArgs
     writeDataToFile file $ Scholarship.ScholarshipRedeemer $ read refundString
+    print(toData $ ScholarshipRedeemer False)
--- code/directed-smart-contract/src/MilestoneToken.hs
+++ code/directed-smart-contract/src/MilestoneToken.hs
@@ -21,13 +21,14 @@ import Data.String
 import Text.Read (readMaybe)
 import Data.List (last)
 import Data.Char
+import Data.Text (pack)
 
 {-# INLINABLE breakAt #-}
 -- Breaks a string into componenets separated by a character. Used assuming tn is in the format pkh#MilestoneNumber
-breakAt :: Char -> String -> [String]
+breakAt :: String -> String -> [String]
 breakAt _ [] = []
-breakAt a (x:xs) 
-  | a == x = []:breakAt a xs
+breakAt a (x:xs)
+  | pack a == pack [x] = []:breakAt a xs
   | otherwise = (x:y):ys
   where
     (y:ys) = breakAt a xs
@@ -35,7 +36,7 @@ breakAt a (x:xs)
 --This minting policy requires that the transaction is signed by the minting institution, and that the token is sent to the pkh specified in the tokenName. Burning is always allowed.
 -- We check the scriptPurpose in order to find the currencySymbol of this script. Then we demand that all minting done with this currencySymbol must either be a burn or signed and sent to specified pkh.
 {-# INLINABLE mkPolicy #-}
-mkPolicy :: Char -> PubKeyHash -> () -> ScriptContext -> Bool
+mkPolicy :: String -> PubKeyHash -> () -> ScriptContext -> Bool
 mkPolicy hashChar instPkh () ctx = all (\(_,tn,n) -> (n<0) || (n>0 && traceIfFalse "not signed by institution" signedByInstitution &&
                                                traceIfFalse "must send to specified pkh" (sentToNamedWallet tn) &&
                                                traceIfFalse "incorrect tn format" (correctTnFormat tn ) )
@@ -54,13 +55,15 @@ mkPolicy hashChar instPkh () ctx = all (\(_,tn,n) -> (n<0) || (n>0 && traceIfFal
     sentToNamedWallet tn = maybe False ((==) (walletByteString tn ) . getPubKeyHash) (maybePkhReciever tn)
 
 {-# INLINABLE mkWrappedPolicy #-}
-mkWrappedPolicy :: Char -> PubKeyHash -> BuiltinData -> BuiltinData -> ()
+mkWrappedPolicy :: String -> PubKeyHash -> BuiltinData -> BuiltinData -> ()
 mkWrappedPolicy a = wrapMintingPolicy . mkPolicy a
 
 policy :: Char -> PubKeyHash -> MintingPolicy
-policy a pkh = mkMintingPolicyScript ($$(compile [|| mkWrappedPolicy ||]) `applyCode` liftCode a `applyCode` liftCode pkh )
+policy a pkh = mkMintingPolicyScript ($$(compile [|| mkWrappedPolicy ||]) `applyCode` liftCode [a] `applyCode` liftCode pkh )
 
 {-# INLINABLE curSymbol #-}
 curSymbol :: PubKeyHash -> CurrencySymbol
 curSymbol = scriptCurrencySymbol . (policy 'b')
 
+-- use bytestrings for this entire flow. 
+-- utf8 encode it.  breakAt should be in plutusTx - sliceByteString. 
